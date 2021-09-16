65646733b5c4db6637cb3980ad14bb1dc7949057
Eric Ahmann <eric@ahmann.me>
2021-09-15 23:59:56 -0500

    ContactUsForm initial placeholder

--- src/components/shared/BlockManager/BlockManager.tsx
+++ src/components/shared/BlockManager/BlockManager.tsx
@@ -1,11 +1,13 @@
 import CallToAction from "@components/blocks/CallToAction"
 import Carousel from "@components/blocks/Carousel"
+import ContactUsForm from "@components/blocks/ContactUsForm"
 import Hero from "@components/blocks/Hero"
 
 const blockComponents = {
   "blocks.hero-v2": Hero,
   "blocks.carousel": Carousel,
   "blocks.call-to-action": CallToAction,
+  "blocks.contact-us-form": ContactUsForm,
 }
 
 const getBlockComponent = ({ __component, ...rest }, index) => {
