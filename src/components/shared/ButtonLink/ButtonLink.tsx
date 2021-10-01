96936ed2deb140ec1910a661a145f82cc5197fcc
Eric Ahmann <eric@ahmann.me>
2021-09-30 23:00:25 -0500

    switch back to border on ButtonLink

--- src/components/shared/ButtonLink/ButtonLink.tsx
+++ src/components/shared/ButtonLink/ButtonLink.tsx
@@ -44,7 +44,7 @@ const ButtonLink: React.FC<Props> = ({ button, className }) => {
     <CustomLink link={button} className={classNames(" text-center", className)}>
       <div
         className={classNames(
-          "px-6 py-3 text-base font-medium rounded-md w-full ",
+          "border-2 px-6 py-3 text-base font-medium rounded-md w-full ",
           {
             "shadow-xl": shadow,
           },
@@ -52,35 +52,35 @@ const ButtonLink: React.FC<Props> = ({ button, className }) => {
             "shadow-none": !shadow,
           },
           {
-            "bg-primary-500 text-white text-opacity-90 hover:text-opacity-100 hover:bg-primary-400":
+            "border-transparent bg-primary-500 text-white text-opacity-90 hover:text-opacity-100 hover:bg-primary-400":
               appearance === "primary",
           },
           {
-            "ring-2 ring-primary-500 bg-gray-50 text-primary-800 hover:bg-primary-25":
+            "border-primary-500 bg-gray-50 text-primary-800 hover:bg-primary-25":
               appearance === "primary-outline",
           },
           {
-            "bg-secondary-500 text-white text-opacity-90 hover:text-opacity-100 hover:bg-secondary-400":
+            "border-transparent bg-secondary-500 text-white text-opacity-90 hover:text-opacity-100 hover:bg-secondary-400":
               appearance === "secondary",
           },
           {
-            "ring-2 ring-secondary-500 bg-gray-50 text-secondary-800 hover:bg-secondary-25":
+            "border-secondary-500 bg-gray-50 text-secondary-800 hover:bg-secondary-25":
               appearance === "secondary-outline",
           },
           {
-            "ring-2 ring-brand-500 bg-brand-500 text-white text-opacity-90 hover:text-opacity-100 hover:bg-brand-400 hover:ring-brand-400":
+            "border-transparent bg-brand-500 text-white text-opacity-90 hover:text-opacity-100 hover:bg-brand-400 hover:ring-brand-400":
               appearance === "brand",
           },
           {
-            "ring-2 ring-brand-500 bg-gray-50 text-brand-800 text-opacity-90 hover:text-opacity-100 hover:bg-brand-25":
+            "border-2 border-brand-500 bg-gray-50 text-brand-800 text-opacity-90 hover:text-opacity-100 hover:bg-brand-25":
               appearance === "brand-outline",
           },
           {
-            "bg-gray-50 text-gray-800 hover:bg-gray-200":
+            "border-transparent bg-gray-50 text-gray-800 hover:bg-gray-200":
               appearance === "muted",
           },
           {
-            "ring-2 ring-gray-500 bg-gray-50 text-gray-800 hover:bg-gray-200":
+            "border-2 border-gray-500 bg-gray-50 text-gray-800 hover:bg-gray-200":
               appearance === "muted-outline",
           }
         )}
